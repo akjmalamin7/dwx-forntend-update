0b51a87e082ca1d9546f6985f4227cf4ae11e942
mahfuj156 <mahfujrahman462@gmail.com>
2026-03-15 05:59:13 +0600

    Profile inactive status added

--- src/shared/redux/features/profile/profileApi.ts
+++ src/shared/redux/features/profile/profileApi.ts
@@ -10,7 +10,7 @@ export const ProfileApi = apiSlice.injectEndpoints({
 
     getProfile: builder.query<ProfileReferenceResponse, string>({
       query: (id) => ({
-        url: `/agent/profile/${id}`,
+        url: `/profile/${id}`,
         method: "GET",
       }),
     }),
--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -2,17 +2,39 @@ import { Outlet } from "react-router-dom";
 import { Footer } from "./footer";
 import { Header } from "./header";
 import ScrollToTop from "./ScrollToTop";
- 
-const Layout = () => {
+import { useGetProfile } from "@/shared/hooks/use-get-profile/useGetProfile";
+import { Loader } from "@/shared/ui";
+import { AgentFormError } from "@/features/agent/agent-form-error"; 
+import { WSProvider } from "@/shared/hooks/use-socket/WSContext";
+
+const LayoutContent = () => {
+  const { status, isProfileLoading } = useGetProfile();
+
+  if (isProfileLoading) return <Loader />;
+
+  if (status === "inactive") {
+    return (
+      <AgentFormError title="Access Denied! Please contact support." />
+    );
+  }
+
   return (
     <div>
       <Header />
-       <ScrollToTop />
+      <ScrollToTop />
       <div className="m-4">
         <Outlet />
       </div>
       <Footer />
     </div>
+  );
+};
+
+const Layout = () => {
+  return (
+    <WSProvider>
+      <LayoutContent />
+    </WSProvider>
   );
 };
 
